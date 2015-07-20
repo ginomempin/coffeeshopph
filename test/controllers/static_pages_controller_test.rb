7344733e4bdc8567fe65b55bb6b50dde6380b3b9
gino <gino.mempin@gmail.com>
2015-07-20 17:54:31 +0800

    - refactored Home, About, and Help pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,18 +3,33 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
 
   test "should get home" do
+    # check GETing the URL for the Home page
     get :home
     assert_response :success
+    # check the <title> tag
+    assert_select "title", "CoffeeShop | Home"
+    # check the page header
+    assert_select "h1", "CoffeeShop"
   end
 
   test "should get help" do
+    # check GETing the URL for the Help page
     get :help
     assert_response :success
+    # check the <title> tag
+    assert_select "title", "CoffeeShop | Help"
+    # check the page header
+    assert_select "h1", "Help"
   end
 
   test "should get about" do
+    # check GETing the URL for the About page
     get :about
     assert_response :success
+    # check the <title> tag
+    assert_select "title", "CoffeeShop | About"
+    # check the page header
+    assert_select "h1", "About"
   end
 
 end
