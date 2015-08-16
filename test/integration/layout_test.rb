30e3d3d9c1cab267d1c52ee9d3f9939298ada798
gino <gino.mempin@gmail.com>
2015-08-16 15:57:57 +0800

    - add integration test for login to signup

--- test/integration/layout_test.rb
+++ test/integration/layout_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class LayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout_links" do
+  test "links_from_home" do
     get root_path
     assert_template "static_pages/home"
     assert_select "a[href=?]", root_path, count: 2
@@ -10,6 +10,12 @@ class LayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path, count: 1
     assert_select "a[href=?]", contact_path, count: 1
     assert_select "a[href=?]", login_path, count: 1
+  end
+
+  test "links_from_login" do
+    get login_path
+    assert_template "users/login"
+    assert_select "a[href=?]", signup_path, count: 1
   end
 
 end
