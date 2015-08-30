8bfaea8d4c3a5390ef6f4b18aaa701ffcdf49115
gino <gino.mempin@gmail.com>
2015-08-30 14:04:22 +0800

    - replace exact string matching with regex

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -13,7 +13,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template "users/new"
     assert_select "div#error_explanation"
-    assert_select "div.alert-danger", "The form contains 4 errors."
+    assert_select "div.alert-danger", text:/4 errors/
     assert_select "div.field_with_errors", count:8
   end
 
