c79d614cc7178f25632c5e00dbcb191f508ce41e
gino <gino.mempin@gmail.com>
2015-10-18 21:46:31 +0800

    - add activation tests - moved user activation from controller to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -56,6 +56,17 @@ class User < ActiveRecord::Base
     update_attribute(:remember_digest, nil)
   end
 
+  # Activates the user.
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends the user activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   #-------------------
   # Class Methods
   #-------------------
