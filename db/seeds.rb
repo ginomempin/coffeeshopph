755e8f991aec433a1b85c1b00fc6bb8cb210f97f
gino <gino.mempin@gmail.com>
2015-11-08 22:28:56 +0800

    - add initial Order model

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,29 @@ User.create!(name:                  "Gino Mempin",
                activated:             true,
                activated_at:          Time.zone.now)
 end
+
+##### ORDERS #####
+
+# served
+1.upto(5) do |n|
+  name = "Order #{n}"
+  price = Random.new.rand(30.0..500.0)
+  quantity = Random.new.rand(1..4)
+  served = true
+  Order.create!(name:     name,
+                price:    price,
+                quantity: quantity,
+                served:   served)
+end
+
+# waiting to be served
+6.upto(10) do |n|
+  name = "Order #{n}"
+  price = Random.new.rand(30.0..500.0)
+  quantity = Random.new.rand(1..4)
+  served = false
+  Order.create!(name:     name,
+                price:    price,
+                quantity: quantity,
+                served:   served)
+end
