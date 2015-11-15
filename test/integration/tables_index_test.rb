0521f7d83094d9f8428fd2357bf5e45f370127b5
gino <gino.mempin@gmail.com>
2015-11-15 22:01:50 +0800

    - update the Order fixtures to associate with Table fixtures - update integration tests for Tables and Orders info page

--- test/integration/tables_index_test.rb
+++ test/integration/tables_index_test.rb
@@ -26,23 +26,4 @@ class TablesIndexTest < ActionDispatch::IntegrationTest
     end
   end
 
-  test "should display table info" do
-    #1. login
-    check_log_in_as(@admin1)
-    #2. access the tables info page
-    get table_path(@table1.id)
-    assert_template "tables/show"
-    #3. check the contents of the page
-    assert_select "h1", { text: @table1.name }
-    assert_select "span.badge", { text: "#{@table1.max_persons}",
-                                  count: 1 }
-    assert_select "span.badge", { text: "#{@table1.num_persons}",
-                                  count: 1 }
-    if @table1.occupied?
-      assert_select "span.label", { text: "OCCUPIED" }
-    else
-      assert_select "span.label", { text: "FREE" }
-    end
-  end
-
 end
