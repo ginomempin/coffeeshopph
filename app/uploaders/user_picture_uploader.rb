5c97456262eaacaf4bcf36a95895e57fabccadbb
gino <gino.mempin@gmail.com>
2015-11-19 22:05:05 +0800

    - add cleanup of CarrierWave's tmp directory

--- app/uploaders/user_picture_uploader.rb
+++ app/uploaders/user_picture_uploader.rb
@@ -1,13 +1,26 @@
 # encoding: utf-8
 
 class UserPictureUploader < CarrierWave::Uploader::Base
-
   include CarrierWave::MiniMagick
-  process resize_to_limit: [150, 150]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
+
+  # Delete the file in the tmp directory after it is stored
+  # https://github.com/carrierwaveuploader/carrierwave/wiki/How-to:-Delete-cache-garbage-directories
+  before :store, :remember_cache_id
+  after :store, :delete_tmp_dir
+  def remember_cache_id(new_file)
+    # store! nil's the cache_id after it finishes so we need to remember it for deletion
+    @cache_id_was = cache_id
+  end
+  def delete_tmp_dir(new_file)
+    # make sure we don't delete other things accidentally by checking the name pattern
+    if @cache_id_was.present? && @cache_id_was =~ /\A[\d]{8}\-[\d]{4}\-[\d]+\-[\d]{4}\z/
+      FileUtils.rm_rf(File.join(root, cache_dir, @cache_id_was))
+    end
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -25,6 +38,7 @@ class UserPictureUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
+  process resize_to_limit: [150, 150]
   #
   # def scale(width, height)
   #   # do something
